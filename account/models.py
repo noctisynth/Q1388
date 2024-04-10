6e72a114aa74e99211cbae569c2a252c89f652d1
FreetBash <3141495167@qq.com>
2024-04-10 10:14:52 +0800

    feat:(account):实现了add login update profile
    鉴权使用session

--- account/models.py
+++ account/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class UserAccount(models.Model):
-    username = models.CharField(max_length=30,verbose_name="用户名")
-    password = models.CharField(max_length=50,verbose_name="密码")
-    email = models.EmailField(max_length=254,verbose_name="邮件")
-    avatar = models.CharField(max_length=254,verbose_name="头像")
-    addresses = models.CharField(max_length=2000,verbose_name="地址")
+    username = models.CharField(max_length=30,verbose_name="用户名",unique=True,null=False)
+    password = models.CharField(max_length=50,verbose_name="密码",null=False)
+    email = models.EmailField(max_length=254,verbose_name="邮件", default="")
+    avatar = models.CharField(max_length=254,verbose_name="头像", default="")
+    addresses = models.CharField(max_length=2000,verbose_name="地址", default="")
+    default_address = models.CharField(max_length=200, verbose_name="默认地址", default="")
---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("add", views.add),
+    path("login", views.login),
+    path("update", views.update),
+    path("profile", views.profile)
+]
