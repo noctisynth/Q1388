330f63e1e98cbc28487638ba78731ad6425a83fd
FreetBash <3141495167@qq.com>
2024-04-11 09:34:41 +0800

    feat(order): 已完成，测试已通过

---
+++ src-django/order/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from account.models import UserAccount
+from product.models import Product
+
+
+# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ("Pending", "Pending"),
+        ("Paid", "Paid"),
+        ("Shipped", "Shipped"),
+        ("Completed", "Completed"),
+        ("Cancelled", "Cancelled"),
+    )
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
