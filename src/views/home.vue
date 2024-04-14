4ea6db011b6a7e2a4b09ed235d1dc75cd1b032ed
苏向夜 <fu050409@163.com>
2024-04-14 21:45:35 +0800

    feat(home): refactor date set with apifox

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,29 +8,44 @@ console.log(UserToken.token);
 
 const recommends = ref([
   {
-    id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5
+    "id": 1,
+    "name": "手机",
+    "price": 1000,
+    "quantity": 10,
+    "spec_param": "4G手机",
+    "categories": [
+      "电子产品"
+    ],
+    "comment": "很好用的手机",
+    "detail": "这是一个很好用的手机",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
   },
   {
-    id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5
+    "id": 2,
+    "name": "T恤",
+    "price": 20,
+    "quantity": 50,
+    "spec_param": "纯棉T恤",
+    "categories": [
+      "服装"
+    ],
+    "comment": "舒适的T恤",
+    "detail": "这是一件舒适的T恤",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
   },
+  {
+    "id": 3,
+    "name": "Python编程入门",
+    "price": 50,
+    "quantity": 30,
+    "spec_param": "Python编程入门书籍",
+    "categories": [
+      "图书"
+    ],
+    "comment": "学习编程的好书",
+    "detail": "这是一本Python编程入门的好书",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
+  }
 ]);
 const responsiveOptions = ref([
   {
@@ -52,29 +67,44 @@ const responsiveOptions = ref([
 
 const products = ref([
   {
-    id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5
+    "id": 1,
+    "name": "手机",
+    "price": 1000,
+    "quantity": 10,
+    "spec_param": "4G手机",
+    "categories": [
+      "电子产品"
+    ],
+    "comment": "很好用的手机",
+    "detail": "这是一个很好用的手机",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
   },
   {
-    id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5
+    "id": 2,
+    "name": "T恤",
+    "price": 20,
+    "quantity": 50,
+    "spec_param": "纯棉T恤",
+    "categories": [
+      "服装"
+    ],
+    "comment": "舒适的T恤",
+    "detail": "这是一件舒适的T恤",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
   },
+  {
+    "id": 3,
+    "name": "Python编程入门",
+    "price": 50,
+    "quantity": 30,
+    "spec_param": "Python编程入门书籍",
+    "categories": [
+      "图书"
+    ],
+    "comment": "学习编程的好书",
+    "detail": "这是一本Python编程入门的好书",
+    "pictures": "https://primefaces.org/cdn/primevue/images/galleria/galleria10.jpg"
+  }
 ])
 </script>
 
@@ -89,9 +119,8 @@ const products = ref([
             <div class="border-1 surface-border b-rd m-2 p-3">
               <div class="mb-3">
                 <div class="relative mx-auto">
-                  <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image"
-                    :alt="slotProps.data.name" class="w-full b-rd" />
-                  <Tag :value="slotProps.data.inventoryStatus" :severity="'info'" class="absolute"
+                  <img :src="slotProps.data.pictures" :alt="slotProps.data.name" class="w-full b-rd" />
+                  <Tag :value="slotProps.data.categories[0]" severity="info" class="absolute"
                     style="left:5px; top: 5px" />
                 </div>
               </div>
@@ -99,7 +128,6 @@ const products = ref([
               <div class="flex justify-between items-center">
                 <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>
                 <span>
-                  <Button icon="pi pi-heart" severity="secondary" outlined></Button>
                   <Button icon="pi pi-shopping-cart" class="ml-2"></Button>
                 </span>
               </div>
@@ -113,31 +141,22 @@ const products = ref([
                 <div class="flex flex-col sm:flex-row sm:items-center p-4 gap-3"
                   :class="{ 'b-t-1 surface-border': index !== 0 }">
                   <div class="md:w-10rem relative">
-                    <img class="block xl:block mx-auto border-round w-full"
-                      :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" />
-                    <Tag :value="item.inventoryStatus" :severity="'info'" class="absolute" style="left: 4px; top: 4px">
+                    <img class="block xl:block mx-auto b-rd w-full" :src="item.pictures" :alt="item.name" />
+                    <Tag :value="item.categories[0]" severity="info" class="absolute" style="left: 4px; top: 4px">
                     </Tag>
                   </div>
                   <div class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-4">
                     <div class="flex flex-row md:flex-col justify-between items-start gap-2">
                       <div>
-                        <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
                         <div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
+                        <div class="text-sm mt-2 text-coolGray">{{ item.detail }}</div>
                       </div>
-                      <div class="surface-100 p-1" style="border-radius: 30px">
-                        <div class="surface-0 flex items-center gap-2 justify-center py-1 px-2"
-                          style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
-                          <span class="text-900 font-medium text-sm">{{ item.rating }}</span>
-                          <i class="pi pi-star-fill text-yellow-500"></i>
-                        </div>
-                      </div>
+                      <div></div>
                     </div>
                     <div class="flex flex-col md:items-end gap-5">
                       <span class="text-xl font-semibold text-900">${{ item.price }}</span>
-                      <div class="flex flex-row-reverse md:flex-row gap-2">
-                        <Button icon="pi pi-heart" outlined></Button>
-                        <Button icon="pi pi-shopping-cart" label="Buy Now"
-                          :disabled="item.inventoryStatus === 'OUTOFSTOCK'"
+                      <div class="flex flex-row w-full justify-end">
+                        <Button icon="pi pi-shopping-cart" label="购买"
                           class="flex-auto md:flex-initial whitespace-nowrap"></Button>
                       </div>
                     </div>
