0f8f93b68593aefaf9dc63ec0466beb497b3c427
苏向夜 <fu050409@163.com>
2024-04-14 21:46:15 +0800

    feat(profile): refactor header in profile page

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -122,26 +122,8 @@ onMounted(() => {
 </script>
 
 <template>
-    <main>
-        <Menubar :model="items" class="!border-x-none !b-rd-0" breakpoint="600px">
-            <template #item="{ item, props, hasSubmenu, root }">
-                <a v-ripple class="flex align-items-center" v-bind="props.action">
-                    <span :class="item.icon"></span>
-                    <span class="ml-2">{{ item.label }}</span>
-                    <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
-                    <span v-if="item.shortcut"
-                        class="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">{{
-                            item.shortcut }}</span>
-                    <i v-if="hasSubmenu"
-                        :class="['pi pi-angle-down', { 'pi-angle-down ml-2': root, 'pi-angle-right ml-auto': !root }]"></i>
-                </a>
-            </template>
-            <template #end>
-                <div class="flex align-items-center gap-2">
-                    <InputText placeholder="Search" type="text" class="w-8rem sm:w-auto" />
-                </div>
-            </template>
-        </Menubar>
+    <div>
+        <Header></Header>
 
         <div class="flex justify-center">
             <div class="w-full max-w-960px flex flex-row">
@@ -201,5 +183,7 @@ onMounted(() => {
 
             </div>
         </div>
-    </main>
+
+        <Footer></Footer>
+    </div>
 </template>
