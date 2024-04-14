39cc226bb008c0f5943990b4589bfbfd1a0ec0d9
苏向夜 <fu050409@163.com>
2024-04-14 13:49:01 +0800

    feat(router): use relative import for packages route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,29 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/home.vue";
-import ProfileView from "@/views/profile.vue";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import HomeView from "@/views/home.vue";
+
+const views = import.meta.glob([
+  "../views/**/index.vue",
+  "../views/**/\\[*\\].vue",
+]);
+
+const routes: RouteRecordRaw[] = Object.entries(views).map(
+  ([filePath, component]) => {
+    let path = filePath.replace(/^\.\.\/views\//, "");
+    path = path.replace(/^(.+)\/index\.vue$/, "$1");
+    path = path.replace(/\[(\w+)\]\.vue$/, ":$1");
+    path = "/" + path;
+
+    return {
+      path,
+      name: filePath.replace(/^\.\.\/views\//, ""),
+      component,
+    } satisfies RouteRecordRaw;
+  }
+);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +33,7 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    {
-      path: "/profile",
-      name: "profile",
-      component: ProfileView,
-    },
+    ...routes,
   ],
 });
 
