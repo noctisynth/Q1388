d75f51d879741e35fb8560b073ef24916fbeebe0
苏向夜 <fu050409@163.com>
2024-04-15 11:57:20 +0800

    feat(register): add register panel

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,8 +9,11 @@ const router = useRouter()
 const tokenStore = useTokenStore()
 const toast = useToast()
 const showLogin = ref<boolean>(false)
+const showRegister = ref<boolean>(false)
 const username = ref<string | null>()
+const email = ref<string | null>()
 const password = ref<string | null>()
+const password_confirm = ref<string | null>()
 
 let user_items;
 if (!tokenStore.isLoggedIn())
@@ -20,13 +23,15 @@ if (!tokenStore.isLoggedIn())
             icon: 'pi pi-sign-in',
             command: () => {
                 showLogin.value = true
+                showRegister.value = false
             }
         },
         {
             label: '注册',
             icon: 'pi pi pi-plus-circle',
             command: () => {
-                showLogin.value = true
+                showRegister.value = true
+                showLogin.value = false
             }
         }
     ]
@@ -34,9 +39,16 @@ else
     user_items = [
         {
             label: '个人中心',
-            icon: 'pi user',
+            icon: 'pi pi-user',
             command: () => {
                 router.push("/profile")
+            }
+        },
+        {
+            label: '购物车',
+            icon: 'pi pi-shopping-cart',
+            command: () => {
+                router.push("/cart")
             }
         },
     ]
@@ -66,7 +78,8 @@ const items = ref([
 async function login() {
     const res = await axios.post("/account/login", {
         username: username.value,
-        password: password.value
+        password: password.value,
+        token: 123,
     })
     if (res.data.status === 200) {
         tokenStore.setToken(res.data.token)
@@ -77,11 +90,37 @@ async function login() {
         window.location.reload();
         showLogin.value = false
     } else {
+        console.log(res.data)
+        toast.add({
+            'severity': 'error', 'summary': '失败', 'detail': "登录失败: 账号或密码错误！", 'life': 3000
+        })
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+    }
+}
+
+async function register() {
+    if (password.value !== password_confirm.value) {
+        return toast.add({
+            'severity': 'error', 'summary': '失败', 'detail': "两次输入的密码不一致", 'life': 3000
+        })
+    }
+    const res = await axios.post("/account/add", {
+        username: username.value,
+        password: password.value,
+        email: email.value
+    })
+    if (res.data.status === 200) {
+        toast.add({
+            'severity': 'success', 'summary': '成功', 'detail': res.data.message, 'life': 3000
+        })
+        await new Promise((resolve) => setTimeout(resolve, 3000))
+        window.location.reload();
+        showRegister.value = false
+    } else {
         toast.add({
             'severity': 'error', 'summary': '失败', 'detail': res.data.message, 'life': 3000
         })
         await new Promise((resolve) => setTimeout(resolve, 3000))
-        showLogin.value = false
     }
 }
 </script>
@@ -102,6 +141,31 @@ async function login() {
         <div class="flex justify-end gap-2">
             <Button type="button" label="取消" severity="secondary" @click="showLogin = false"></Button>
             <Button @click="login" type="button" label="登录"></Button>
+        </div>
+    </Dialog>
+    <Dialog v-model:visible="showRegister" modal header="注册" class="max-w-90% w-xl">
+        <span class="p-text-secondary">注册一个新的账户</span>
+        <div class="flex gap-3 items-center justify-center flex-col w-full p-5">
+            <div class="flex flex-col w-full max-w-70% px-1rem gap-1rem">
+                <label for="username" class="font-semibold">用户名</label>
+                <InputText v-model="username" id="username" class="w-full" />
+            </div>
+            <div class="flex flex-col w-full max-w-70% px-1rem gap-1rem">
+                <label for="email" class="font-semibold">邮箱</label>
+                <InputText v-model="email" id="email" class="w-full" />
+            </div>
+            <div class="flex flex-col w-full max-w-70% px-1rem gap-1rem">
+                <label for="password" class="font-semibold">密码</label>
+                <Password v-model="password" inputId="password" inputClass="w-full" class="w-full" />
+            </div>
+            <div class="flex flex-col w-full max-w-70% px-1rem gap-1rem">
+                <label for="password-confirm" class="font-semibold">密码确认</label>
+                <Password v-model="password_confirm" inputId="password-confirm" inputClass="w-full" class="w-full" />
+            </div>
+        </div>
+        <div class="flex justify-end gap-2">
+            <Button type="button" label="取消" severity="secondary" @click="showRegister = false"></Button>
+            <Button @click="register" type="button" label="登录"></Button>
         </div>
     </Dialog>
     <Menubar :model="items" class="!border-x-none !b-rd-0" breakpoint="600px">
